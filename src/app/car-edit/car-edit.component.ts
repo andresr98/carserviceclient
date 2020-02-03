92bc6380783a96b35b26a0f57fe245ba461d61ac
Andres Ruiz Graciano <aandres.r98@gmail.com>
2020-02-02 19:31:56 -0500

    Car list now show owner name

--- src/app/car-edit/car-edit.component.ts
+++ src/app/car-edit/car-edit.component.ts
@@ -38,7 +38,6 @@ export class CarEditComponent implements OnInit, OnDestroy {
         this.carService.get(id).subscribe((car: any) => {
           if (car) {
             this.car = car;
-            console.log(this.car);
             this.car.href = car._links.self.href;
             this.giphyService.get(car.name).subscribe(url => car.giphyUrl = url);
           }
