2cb51f8a75781f788091785feb16d291ea7b9673
Andres Ruiz <aandres.r98@gmail.com>
2020-03-05 10:44:05 -0500

    added multiple owners delete and refactor service owner

--- src/app/owner-list/owner-list.component.ts
+++ src/app/owner-list/owner-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { OwnerService } from '../shared/owner/owner.service'
+import { OwnerService } from '../shared/owner/owner.service';
 
 @Component({
   selector: 'app-owner-list',
@@ -9,16 +9,42 @@ import { OwnerService } from '../shared/owner/owner.service'
 export class OwnerListComponent implements OnInit {
 
   owners = [];
+  selecteds = 0;
+  indexOwners = new Map();
 
   constructor(private ownerService: OwnerService) { }
 
   ngOnInit() {
     this.ownerService.getAllOwners().subscribe(data => {
       this.owners = data._embedded.owners;
+
+      for (const owner of this.owners) {
+        owner.href = owner._links.owner.href;
+        owner.checked = false;
+      }
     },
       error => {
         console.log('No se puede traer los Owners');
       });
   }
 
+  changeCheckBoxEvent(event, index) {
+    if (event.checked === true) {
+      this.selecteds++;
+      this.indexOwners.set(index, index);
+    } else {
+      this.selecteds--;
+      this.indexOwners.delete(index);
+    }
+  }
+
+  deleteOwners() {
+    this.indexOwners.forEach(key => {
+      const owner = this.owners[key];
+
+      this.ownerService.removeRelation(owner.dni);
+
+      this.ownerService.deleteOwnerByHref(owner.href)
+    });
+  }
 }
